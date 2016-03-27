80c678e55f313425b9eb60fbddd36a1ab24b728f
notSherry <1204800782@qq.com>
2016-03-27 17:34:30 +0800

    PM 方程降噪

--- main.cpp
+++ main.cpp
@@ -7,58 +7,53 @@ using namespace cv;
 
 typedef double ty;
 
-void PMenhence::gradn(double*A,double*B)    //求N方向梯度
+void PMenhence::gradn(Mat &A,Mat &B)    //求N方向梯度
 {
     int h, w;
     for (h = 1; h < Y_image; h++)
         for (w = 0; w < X_image; w++) {
-//            B.at<ty>(h, w) = A.at<ty>(h - 1, w) - A.at<ty>(h, w);
-            B[h*X_image+w] = A[(h-1)*X_image+w]-A[h*X_image+w];
+            B.at<ty>(h, w) = A.at<ty>(h - 1, w) - A.at<ty>(h, w);
         }
 }
 
-void PMenhence::grads(double*A, double*B)    //求S方向梯度
+void PMenhence::grads(Mat &A, Mat &B)    //求S方向梯度
 {
     int h, w;
     for (h = 0; h < Y_image - 1; h++)
         for (w = 0; w < X_image; w++) {
-//            B.at<ty>(h, w) = A.at<ty>(h + 1, w) - A.at<ty>(h, w);
-            B[h*X_image+w] = A[(h+1)*X_image+w]-A[h*X_image+w];
+            B.at<ty>(h, w) = A.at<ty>(h + 1, w) - A.at<ty>(h, w);
         }
 }
 
-void PMenhence::grade(double*A, double*B)    //求E方向梯度
+void PMenhence::grade(Mat &A, Mat &B)    //求E方向梯度
 {
     int h, w;
     for (h = 0; h < Y_image; h++)
         for (w = 0; w < X_image - 1; w++) {
-//            B.at<ty>(h, w) = A.at<ty>(h, w + 1) - A.at<ty>(h, w);
-            B[h*X_image+w] = A[h*X_image+w+1]-A[h*X_image+w];
+            B.at<ty>(h, w) = A.at<ty>(h, w + 1) - A.at<ty>(h, w);
         }
 }
 
-void PMenhence::gradw(double*A, double*B)    //求W方向梯度
+void PMenhence::gradw(Mat &A, Mat &B)    //求W方向梯度
 {
     int h, w;
     for (h = 0; h < Y_image; h++)
         for (w = 1; w < X_image; w++) {
-//            B.at<ty>(h, w) = A.at<ty>(h, w - 1) - A.at<ty>(h, w);
-            B[h*X_image+w] = A[h*X_image+w-1]-A[h*X_image+w];
+            B.at<ty>(h, w) = A.at<ty>(h, w - 1) - A.at<ty>(h, w);
         }
 }
 
-void PMenhence::pm1_diffusion(double*A, double*B,
+void PMenhence::pm1_diffusion(Mat &A, Mat &B,
                               double k)    //按照第一个公式求扩散系数'pm1': perona-malik, c=exp{-(|grad(J)|/K)^2} [PM90]
 {
     int h, w;
     for (h = 0; h < Y_image; h++)
         for (w = 0; w < X_image; w++) {
-//            B.at<ty>(h, w) = exp(-(abs(A.at<ty>(h, w)) / k) * (abs(A.at<ty>(h, w)) / k));
-            B[h*X_image+w]=exp(-(abs(A[h*X_image+w])/k)*(abs(A[h*X_image+w])/k));
+            B.at<ty>(h, w) = exp(-(abs(A.at<ty>(h, w)) / k) * (abs(A.at<ty>(h, w)) / k));
         }
 }
 
-void PMenhence::pm2_diffusion(double*A,double*B,
+void PMenhence::pm2_diffusion(Mat &A,Mat &B,
                               double k)    //按照第二个公式求扩散系数'pm2': perona-malik, c=1/{1+(|grad(J)|/K)^2} [PM90]
 {
     int h, w;
@@ -66,16 +61,37 @@ void PMenhence::pm2_diffusion(double*A,double*B,
     int b = Y_image;
     for (h = 0; h < Y_image; h++)
         for (w = 0; w < X_image; w++) {
-//            B.at<ty>(h, w) = 1 / ((abs(A.at<ty>(h, w)) / k) * (abs(A.at<ty>(h, w)) / k) + 1);
-            B[h*X_image+w]=1/((abs(A[h*X_image+w])/k)*(abs(A[h*X_image+w])/k)+1);
+            B.at<ty>(h, w) = 1 / ((abs(A.at<ty>(h, w)) / k) * (abs(A.at<ty>(h, w)) / k) + 1);
         }
 }
 
 
+//***********************************************************************************//
+//求最大梯度模和最小梯度模
+//***********************************************************************************//
+
+void PMenhence::get_max_min_grad(Mat&A,double *max,double *min){
+
+}
 
 
 //***********************************************************************************//
-// 函数名称: DiffusionPic(img, method, feed, loop, K, dt, Bmp_head)
+//求增强后的梯度图像
+//***********************************************************************************//
+void PMenhence::enhencegrad(Mat&A,Mat&B){
+    double max,min;
+    int h,w;
+    for(h = 0;h < Y_image;h++){
+        for(w = 0;w < X_image;w++){
+
+        }
+
+    }
+}
+
+
+//***********************************************************************************//
+// 函数名称: DiffusionPic(img, method, feed, loop, K, dt)
 // 函数功能: 对图像进行Perona-Malik 扩散
 // 参数说明:
 //			img:输入图像（以矩阵形式）
@@ -86,7 +102,7 @@ void PMenhence::pm2_diffusion(double*A,double*B,
 //			loop:叠代循环的次数
 //			K:扩散系数的压迫因子
 //			dt:时间增量参数
-//			Bmp_head:24位BMP文件的文件头，用于图像文件输出
+//
 //
 //***********************************************************************************//
 
@@ -94,55 +110,23 @@ void PMenhence::DiffusionPic(Mat &src, int method, int feed, int loop, double K,
     int h, w;
     int n = 0;
     double is = 0, in = 0, iw = 0, ie = 0, cn = 0, cs = 0, cw = 0, ce = 0, sum = 0;
-    double *img;
-    img = new double[X_image*Y_image*4];
-    for(h = 0;h<Y_image;h++){
-        for(w = 0;w<X_image;w++){
-            img[h*X_image+w]=src.at<ty>(h,w);
-        }
-    }
 
 
-    double * IMG;
-
-    double * pIN;
-    double * IS;
-    double * IW;
-    double * IE;
-    double * CN;
-    double * CS;
-    double * CW;
-    double * CE;
-
-    IMG = new double[X_image* Y_image];
-    pIN = new double[X_image* Y_image];
-    IS = new double[X_image* Y_image];
-    IW = new double[X_image* Y_image];
-    IE = new double[X_image* Y_image];
-    CN = new double[X_image* Y_image];
-    CS = new double[X_image* Y_image];
-    CW = new double[X_image* Y_image];
-    CE = new double[X_image* Y_image];
-//    Mat IMG, pIN, IS, IW, IE, CN, CS, CW, CE;
-//    IMG.create(src.size(), src.type());
-//    pIN.create(src.size(), src.type());
-//    IS.create(src.size(), src.type());
-//    IW.create(src.size(), src.type());
-//    IE.create(src.size(), src.type());
-//    CN.create(src.size(), src.type());
-//    CS.create(src.size(), src.type());
-//    CW.create(src.size(), src.type());
-//    CE.create(src.size(), src.type());
+    Mat IMG, pIN, IS, IW, IE, CN, CS, CW, CE;
+    IMG.create(src.size(), src.type());
+    pIN.create(src.size(), src.type());
+    IS.create(src.size(), src.type());
+    IW.create(src.size(), src.type());
+    IE.create(src.size(), src.type());
+    CN.create(src.size(), src.type());
+    CS.create(src.size(), src.type());
+    CW.create(src.size(), src.type());
+    CE.create(src.size(), src.type());
 
 
     for (h = 0; h < Y_image; h++)
         for (w = 0; w < X_image; w++)
-//            IMG.at<ty>(h, w) = src.at<ty>(h , w );
-//            IMG.at<ty>(h, w) = src.at<ty>(h * 4, w * 4);
-//            IMG[h*X_image+w]= img[h*X_image*4+w*4];
-            IMG[h*X_image+w]= img[h*X_image+w];
-
-//    cout<< src.at<ty>(1000,1000)<<"  ";
+            IMG.at<ty>(h, w) = src.at<ty>(h , w );
 
 
 
@@ -151,8 +135,6 @@ void PMenhence::DiffusionPic(Mat &src, int method, int feed, int loop, double K,
     grade(IMG, IE);        //求E方向的梯度
     gradw(IMG, IW);        //求W方向的梯度
 
-//    namedWindow("testforimg");
-//    imshow("testforimg",src);
 
 
     switch (method) {
@@ -176,21 +158,16 @@ void PMenhence::DiffusionPic(Mat &src, int method, int feed, int loop, double K,
     {
         for (h = 1; h < Y_image - 1; h++)        //计算各个方向梯度
             for (w = 1; w < X_image - 1; w++) {
-//                in = IMG.at<ty>(h - 1, w) - IMG.at<ty>(h, w);
-//                is = IMG.at<ty>(h + 1, w) - IMG.at<ty>(h, w);
-//                iw = IMG.at<ty>(h, w - 1) - IMG.at<ty>(h, w);
-//                ie = IMG.at<ty>(h, w + 1) - IMG.at<ty>(h, w);
-                in = IMG[(h-1)*X_image+w]-IMG[h*X_image+w];
-                is = IMG[(h+1)*X_image+w]-IMG[h*X_image+w];
-                iw = IMG[h*X_image+w-1]-IMG[h*X_image+w];
-                ie = IMG[h*X_image+w+1]-IMG[h*X_image+w];
+                in = IMG.at<ty>(h - 1, w) - IMG.at<ty>(h, w);
+                is = IMG.at<ty>(h + 1, w) - IMG.at<ty>(h, w);
+                iw = IMG.at<ty>(h, w - 1) - IMG.at<ty>(h, w);
+                ie = IMG.at<ty>(h, w + 1) - IMG.at<ty>(h, w);
 
                 switch (feed)        //选择扩散系数是否接受反馈的数据更新
                 {
                     case 0:        //不接受反馈数据更新
-//                        sum = in * CN.at<ty>(h, w) + is * CS.at<ty>(h, w) + iw * CW.at<ty>(h, w) +
-//                              ie * CE.at<ty>(h, w);
-                        sum = in*CN[h*X_image+w] + is*CS[h*X_image+w] + iw*CW[h*X_image+w] + ie*CE[h*X_image+w];
+                        sum = in * CN.at<ty>(h, w) + is * CS.at<ty>(h, w) + iw * CW.at<ty>(h, w) +
+                              ie * CE.at<ty>(h, w);
                         break;
                     case 1:        //接受反馈数据更新
                         cn = 1 / (1 + (abs(in) / K) * (abs(in) / K));
@@ -205,45 +182,17 @@ void PMenhence::DiffusionPic(Mat &src, int method, int feed, int loop, double K,
                 }
 
                 sum = sum * dt;        //乘时间增量因子
-//                IMG.at<ty>(h, w) = IMG.at<ty>(h, w) + sum;//更新图像数据
-                IMG[h*X_image+w]=IMG[h*X_image+w]+sum;
+                IMG.at<ty>(h, w) = IMG.at<ty>(h, w) + sum;//更新图像数据
             }
     }
 
 //    cout<<X_image<<" "<<Y_image<<endl;
     for (h = 0; h < Y_image; h++)
         for (w = 0; w < X_image; w++) {
-//            src.at<ty>(h , w ) = IMG.at<ty>(h, w);
-//            src.at<ty>(h * 4, w * 4) = IMG.at<ty>(h, w);
-//            src.at<ty>(h * 4, w * 4 + 1) = IMG.at<ty>(h, w);
-//            src.at<ty>(h * 4, w * 4 + 2) = IMG.at<ty>(h, w);
-//            img[h*X_image*4+w*4]=IMG[h*X_image+w];
-//            img[h*X_image*4+w*4+1]=IMG[h*X_image+w];
-//            img[h*X_image*4+w*4+2]=IMG[h*X_image+w];
-            img[h*X_image+w]=IMG[h*X_image+w];
+            src.at<ty>(h , w ) = IMG.at<ty>(h, w);
 
         }
 
-//    cout<<"come here"<<endl;
-
-
-
-    for(h = 0;h < Y_image;h++){
-        for(w = 0;w < X_image;w++){
-            src.at<ty>(h,w) = img[h*X_image+w];
-        }
-    }
-
-    delete [] IMG;
-    delete [] pIN;
-    delete [] IS;
-    delete [] IW;
-    delete [] IE;
-    delete [] CN;
-    delete [] CS;
-    delete [] CW;
-    delete [] CE;
-    delete [] img;
     return;
 }
 
@@ -282,11 +231,11 @@ int main() {
 
     int feed = 1;
 //    loop:叠代循环的次数
-    int loop = 20;
+    int loop = 3;
 //    K:扩散系数的压迫因子
-    double k = 15;
+    double k = 1;
 //    dt:时间增量参数
-    double dt = 0.15;
+    double dt = 0.25;
 //
 //    int method = 1;
 //    int feed = 0;
